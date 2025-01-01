50f492f9c201529e4fe04c600a4f5d41e7763177
zo-no <2742160682@qq.com>
2025-01-01 14:49:52 +0800

    删除原有代码

--- packages/react-reconciler/src/workTags.ts
+++
@@ -1,8 +0,0 @@
-export type WorkTag =
-	| typeof FunctionComponent
-	| typeof HostRoot
-	| typeof HostComponent;
-
-export const FunctionComponent = 0;
-export const HostRoot = 3;
-export const HostComponent = 5;
